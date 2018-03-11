0d8b09c273ab1f6128f9e70437719b17429a17e8
Sergey Elsukov <oct8cat@gmail.com>
2018-03-12 02:31:31 +0300

    Add modules: `http`, `error`

--- test/index.js
+++ test/index.js
@@ -3,43 +3,41 @@
 const assert = require('assert')
 const {
   env,
-  db: { getUserModel, disconnectDb, createDb, connectDb }
+  db: { getUserModel, disconnectDb, createDb, connectDb, generateUserToken },
+  http: { createServer, startServer, stopServer, createUserJWT }
 } = require('..')
 
 describe('db', () => {
-  let db
-  beforeEach(() => connectDb((db = createDb())))
-  afterEach(() => disconnectDb(db))
-
   it('createDb', () => {
-    assert.equal(db.constructor.name, 'Mongoose')
+    assert.equal(createDb().constructor.name, 'Mongoose')
   })
-  it('disconnectDb', () => {
-    assert.equal(db.connection.readyState, 1)
-    return disconnectDb(db).then((db) => {
-      assert.equal(db.connection.readyState, 0)
-    })
-  })
-  it('connectDb', () => {
+  it('connectDb, disconnectDb', () => {
     const db = createDb()
     assert.equal(db.connection.readyState, 0)
     return connectDb(db)
       .then((db) => assert.equal(db.connection.readyState, 1))
       .then(() => disconnectDb(db))
+      .then(() => assert.equal(db.connection.readyState, 0))
   })
-  it('getUserModel().create', () => {
-    const displayName = 'test'
-    const oauth = { provider: 'test', id: 'test' }
-    const userModel = getUserModel(db)
-    return userModel
-      .remove({ displayName })
-      .then(() => userModel.create({ displayName, oauths: [oauth] }))
-      .then((user) => {
-        assert.equal(user.displayName, displayName)
-        assert.equal(user.oauths.length, 1)
-        assert.equal(user.oauths[0].provider, oauth.provider)
-        assert.equal(user.oauths[0].id, oauth.id)
-      })
+  describe('With `db` created and connected', () => {
+    let db
+    beforeEach(() => connectDb((db = createDb())))
+    afterEach(() => disconnectDb(db))
+
+    it('getUserModel(db).create', () => {
+      const displayName = 'test'
+      const oauth = { provider: 'test', id: 'test' }
+      const userModel = getUserModel(db)
+      return userModel
+        .remove({ displayName })
+        .then(() => userModel.create({ displayName, oauths: [oauth] }))
+        .then((user) => {
+          assert.equal(user.displayName, displayName)
+          assert.equal(user.oauths.length, 1)
+          assert.equal(user.oauths[0].provider, oauth.provider)
+          assert.equal(user.oauths[0].id, oauth.id)
+        })
+    })
   })
 })
 
@@ -49,5 +47,20 @@ describe('env', () => {
   })
   it('getDbURI', () => {
     assert.ok(env.getDbURI().match(/test/))
+  })
+})
+
+describe('http', () => {
+  it('createServer', () => {
+    assert.ok(createServer)
+  })
+  it('startServer', () => {
+    const server = createServer()
+    return startServer(server).then(() => stopServer(server))
+  })
+  it('createUserJWT', () => {
+    const user = { id: 'test' }
+    const token = createUserJWT(user)
+    assert.ok(token)
   })
 })
